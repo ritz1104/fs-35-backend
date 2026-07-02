3490991d5fa53d77289e117a91320437e30c0730
Devendradhote001 <ddhote780@gmail.com>
2026-07-02 18:15:19 +0530

    sdkbhfsd

--- auth/src/routes/home.routes.js
+++ auth/src/routes/home.routes.js
@@ -1,32 +1,38 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
+const UserModel = require("../models/user.model");
 
 const router = express.Router();
 
 router.get(
   "/",
-  (req, res, next) => {
+  async (req, res, next) => {
     const token = req.cookies.secret;
 
     if (!token)
-      return res.status(401).json({
+      return res.status(404).json({
         success: false,
-        message: "token not found",
+        message: "Token not found",
       });
 
     const decode = jwt.verify(token, process.env.JWT_SECRET_KEY);
 
     if (!decode)
-      return res.status(401).json({
+      return res.status(400).json({
         success: false,
-        message: "Invalid user",
+        message: "Invalid token",
       });
+
+    const user = await UserModel.findById(decode.id);
+
+    req.user = user;
 
     next();
   },
   (req, res) => {
     res.status(200).json({
       message: "I m home route",
+      data: req.user,
     });
   }
 );
